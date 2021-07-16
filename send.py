c81f52648365f627df39990a48751dd6ff7468d4
lewamlyn <62939906+lewamlyn@users.noreply.github.com>
2021-07-16 19:52:10 +0800

    Update send.py

--- send.py
+++ send.py
@@ -1,6 +1,7 @@
 import requests
 from urllib import parse
 import sys
+from xpinyin import Pinyin
 
 def send2wechat(send_key,content):
     api = "https://sc.ftqq.com/" + send_key + ".send"
@@ -23,17 +24,25 @@ def get_update(file_name):
             print('无本地存储信息')
     return old
 
+def get_url(update):
+    p = Pinyin() 
+    name = parse.unquote(update).split('-')[0]
+    url = 'https://copymanga.net/comic/' + p.get_pinyin(name,'')
+    return url
+
 if __name__ == '__main__':
     update_lists = get_update('update.txt')
+    print(update_lists)
     content = ''
     if len(update_lists) > 0:
         for update in update_lists:
-            content = content + parse.unquote(update) + '<br>'
+            content =  content + '## ' +  '[' +  parse.unquote(update) + '](' + get_url(update)  + ')\n'
         print(content)
-        print('已向微信发送更新信息')
         try:
             send2wechat(str(sys.argv[1]),content)
+            print('已向微信发送更新信息')
         except:
+            print(content)
             print('发送失败')
     else:
         print('无更新')        
